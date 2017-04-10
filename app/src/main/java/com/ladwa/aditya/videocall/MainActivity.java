56ed608dd28acc9c415046797a8124170eecb225
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-04-10 14:08:23 +0530

    fect(MainActivity): Added Permission logic

--- app/src/main/java/com/ladwa/aditya/videocall/MainActivity.java
+++ app/src/main/java/com/ladwa/aditya/videocall/MainActivity.java
@@ -1,13 +1,48 @@
 package com.ladwa.aditya.videocall;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tbruyelle.rxpermissions.RxPermissions;
+
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TWILIO_ACCESS_TOKEN = "TWILIO_ACCESS_TOKEN";
+    private static final String TAG = "VideoActivity";
+
+
+    private static boolean permissionGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        grantPermission();
+        if (permissionGranted) {
+            startVideo();
+        }
+    }
+
+    private void startVideo() {
+
+    }
+
+    private void grantPermission() {
+        final RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.request(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
+                .subscribe(new Action1<Boolean>() {
+                    @Override
+                    public void call(Boolean granted) {
+                        if (granted) {
+                            permissionGranted = true;
+                        } else {
+                            permissionGranted = false;
+                        }
+                    }
+                });
+
     }
 }
